b16ebd7e191f0d8d41df4ad4eb90e62d894207b8
tylerlamm <tyler.l.jiafu@gmail.com>
2026-09-22 15:15:28 -0600

    Added Scene Template

---
+++ scene/sketch.js
@@ -0,0 +1,15 @@
+// Interactive Scene
+// Tyler Lam
+// September 22, 2026
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+async function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
