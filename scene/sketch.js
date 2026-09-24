5253088c57b27f1834189d4008db7c14b90db00f
tylerlamm <tyler.l.jiafu@gmail.com>
2026-09-24 13:04:50 -0600

    Start of interactive scene (cookie clicker)

--- scene/sketch.js
+++ scene/sketch.js
@@ -1,15 +1,30 @@
-// Interactive Scene
+// Interactive Scene: Random Cookie Clicker
 // Tyler Lam
 // September 22, 2026
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let bg;
+let font;
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
+  bg = await loadImage('images/plains.png');
+  font = await loadFont()
 }
 
 function draw() {
-  background(220);
+  background(bg);
+  mainTitle();
+  //background with title "click the cookie"
+  //have cookie spawn somewhere on the screen
+  //number of times clicked showing somewhere
+  //if cookie clicked +1 to number
+}
+
+function mainTitle() {
+  textSize(64);
+  fill(0);
+  textAlign(CENTER, CENTER)
+  text("click the cookie!", windowWidth/2, windowHeight/6);
 }
